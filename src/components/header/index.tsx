ef628f8e0b82bdd5d2267fdd73290766d1543068
Ejaz <73548455+ejaz4@users.noreply.github.com>
2023-09-20 09:49:43 +0000

    Header fix up

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,8 @@
+import styles from "./Header.module.css"
+
 export const Header = () => {
     return (
-        <div>
+        <div className={styles.header}>
             <h1>Header</h1>
         </div>
     )
