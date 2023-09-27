196a4b52a7f066ae8b6c7cf78a73e8d426e169a6
Ejaz <73548455+ejaz4@users.noreply.github.com>
2023-09-27 09:47:48 +0000

    CSS

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,12 +1,15 @@
-import styles from "./Login.module.css"
+import styles from "./Login.module.css";
 
 export const LoginDialogue = () => {
-    return (
-      <div className={styles.dialogue}>
-        <div className={styles.text}>
-            <h1>Enter your credentials</h1>
-        </div>
+  return (
+    <div className={styles.dialogue}>
+      <div className={styles.text}>
+        <h1>Enter your credentials</h1>
+        <p>Enter your email address</p>
       </div>
-    )
-  }
-  
+
+      <input type="email" id="emailInput" />
+      <input type="password" id="passwordInput" />
+    </div>
+  );
+};
